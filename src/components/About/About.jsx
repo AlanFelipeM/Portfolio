c884223125b54b412063eec96e62323894fd0c3d
Alan Felipe <alanfelipe1678@gmail.com>
2024-07-31 19:48:16 -0300

    correcting about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,7 @@ export default function About () {
         <div className="about">
         <div className='txt-about'>
         <h2>Olá,</h2>
-        <p>Meu nome é Alan Felipe e sou um jovem apaixonado por tecnologia, estudante de Engenharia da Computação e aluno da OnebitCode. <br/> Atualmente sou desenvolvedor Front-End e UI/UX Designer mas estou em constante estudo e aprendizado para me tronar um desenvolvedor FullStack. </p>
+        <p>Meu nome é Alan Felipe e sou um jovem apaixonado por tecnologia, estudante de Engenharia da Computação e aluno da OnebitCode. <br/> Atualmente sou desenvolvedor Front-End e UI/UX Designer mas estou em constante estudo e aprendizado para me tornar um desenvolvedor FullStack. </p>
         <div className='saying'>
         <p><i><span>"  </span>Tente uma, duas, três vezes e se possível tente a quarta, a quinta e quantas vezes for necessário. Só não desista nas primeiras tentativas, a persistência é amiga da conquista. Se você quer chegar aonde a maioria não chega, faça o que a maioria não faz.<span>  "</span></i></p>
         <span className='ass'><i>-Bill Gates</i></span>
