fdb1280420491167bd0a3cf472fa1e3788859aa1
Alan Felipe <alanfelipe1678@gmail.com>
2026-02-24 22:25:10 -0300

    feat: overhaul skills section with kategorized grid, percentages, and high-quality icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Container from 'react-bootstrap/Container'
 import html from '/src/img/html.png'
 import css from '/src/img/css.png'
@@ -7,7 +8,7 @@ import sass from '/src/img/sass.png'
 import bootstrap from '/src/img/bootstrap.png'
 import figma from '/src/img/figma.png'
 import git from '/src/img/git.png'
-import bi from '/src/img/bi.png'
+import github from '/src/img/github.png'
 import ts from '/src/img/ts.png'
 import next from '/src/img/next.png'
 import node from '/src/img/node.png'
@@ -17,100 +18,140 @@ import csharp from '/src/img/csharp.png'
 import dotnet from '/src/img/dotnet.png'
 import Tilt from 'react-parallax-tilt'
 
+import { useTranslation } from 'react-i18next';
+import {
+    FaJava, FaAws, FaDocker, FaCode
+} from 'react-icons/fa';
+import {
+    SiSupabase, SiVite, SiMongodb
+} from 'react-icons/si';
+import {
+    DiSqllite, DiScrum
+} from 'react-icons/di';
+import {
+    TbApi, TbSql
+} from 'react-icons/tb';
+import {
+    BsKanban
+} from 'react-icons/bs';
+
 import '../Skills/Skills.sass'
 
+
 export default function Skills() {
-    return(
-        <>
-        <div className='pdr'>
-        <Container>
-        <h1 className='title'>- Skills -</h1>
-        <div className='skill'>
-            <div className="html1">
-            <Tilt><img src={html} alt="html"/></Tilt>
-            <p>HTML</p>
-            </div>
+    const { t } = useTranslation();
 
-            <div className="tecnologia">
-            <Tilt><img src={css} alt="css"/></Tilt>
-            <p>CSS</p>
-            </div>
+    const skillGroups = [
+        {
+            title: t('cat_frontend'),
+            skills: [
+                { name: 'HTML5', img: html, level: 98 },
+                { name: 'CSS3', img: css, level: 95 },
+                { name: 'JavaScript', img: js, level: 92 },
+                { name: 'TypeScript', img: ts, level: 88 },
+                { name: 'React', img: react, level: 90 },
+                { name: 'Next.js', img: next, level: 85 },
+                { name: 'Vite', icon: <SiVite color="#646CFF" />, level: 90 },
+                { name: 'Sass', img: sass, level: 85 },
+                { name: 'Bootstrap', img: bootstrap, level: 90 },
+                { name: 'Figma', img: figma, level: 80 },
+            ]
+        },
+        {
+            title: t('cat_backend'),
+            skills: [
+                { name: 'Node.js', img: node, level: 80 },
+                { name: 'Java', icon: <FaJava color="#ED8B00" />, level: 75 },
+                { name: 'C#', img: csharp, level: 70 },
+                { name: '.NET', img: dotnet, level: 70 },
+                { name: 'APIs REST', icon: <TbApi color="#4ec9d1" />, level: 85 },
+                { name: 'N8N', icon: <FaCode color="#FF6C37" />, level: 75 },
+            ]
+        },
+        {
+            title: t('cat_database'),
+            skills: [
+                { name: 'MySQL', img: mysql, level: 85 },
+                { name: 'SQL', icon: <TbSql color="#4479A1" />, level: 85 },
+                { name: 'MongoDB', img: mongo, level: 75 },
+                { name: 'Supabase', icon: <SiSupabase color="#3ECF8E" />, level: 75 },
+            ]
+        },
+        {
+            title: t('cat_tools'),
+            skills: [
+                { name: 'Git', img: git, level: 95 },
+                { name: 'GitHub', img: github, level: 95 },
+                { name: 'AWS', icon: <FaAws color="#FF9900" />, level: 60 },
+                { name: 'Docker', icon: <FaDocker color="#2496ED" />, level: 65 },
+                { name: 'Scrum', icon: <DiScrum color="#0096D6" />, level: 95 },
+                { name: 'Kanban', icon: <BsKanban color="#007BFF" />, level: 95 },
+            ]
+        }
+    ];
 
-            <div className="tecnologia">
-            <Tilt><img src={js} alt="js"/></Tilt>
-            <p>JavaScript</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img src={react} alt="react"/></Tilt>
-            <p>React</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img src={sass} alt="sass"/></Tilt>
-            <p>Sass</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img src={bootstrap} alt="bootstrap"/></Tilt>
-            <p>Bootstrap</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img src={git} alt="git"/></Tilt>
-            <p>Git</p>
-            </div>
-           
-            <div className="tecnologia">
-            <Tilt><img className='figma' src={figma} alt="figma"/></Tilt>
-            <p>Figma</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img className='bi' src={bi} alt="bi"/></Tilt>
-            <p>PowerBI</p>
-            </div>
+    return (
+        <div className='skills-page'>
+            <Container className="skills-container">
+                <h1 className='title'>// {t('habilidades_titulo')}</h1>
+
+                <div className="skills-sections-wrapper">
+                    {skillGroups.map((group, idx) => (
+                        <div key={idx} className='skills-section'>
+                            <h2 className="section-subtitle">{group.title}</h2>
+                            <div className='skills-grid'>
+                                {group.skills.map((skill, sIdx) => (
+                                    <SkillItem
+                                        key={sIdx}
+                                        img={skill.img}
+                                        icon={skill.icon}
+                                        name={skill.name}
+                                        level={skill.level}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </Container>
         </div>
+    )
+}
 
-        <div className="studing">
-            
-            <div className="ts">
-         <Tilt> <img src={ts} alt="ts"/></Tilt>
-            <p>TypeScript</p>
-            </div>
+function SkillItem({ img, icon, name, level }) {
+    return (
+        <div className="skill-card">
+            <Tilt
+                tiltMaxAngleX={15}
+                tiltMaxAngleY={15}
+                perspective={1000}
+                scale={1.1}
+                transitionSpeed={1500}
+            >
+                <div className="skill-icon-wrapper">
+                    {img ? (
+                        <img src={img} alt={name} />
+                    ) : icon ? (
+                        <div className="skill-icon-svg">{icon}</div>
+                    ) : (
+                        <div className="skill-placeholder">
+                            <span>{name.charAt(0)}</span>
+                        </div>
+                    )}
 
-            <div className="tecnologia">
-           <Tilt> <img src={next} alt="next"/></Tilt>
-            <p>NextJS</p>
-            </div>
-
-            <div className="tecnologia">
-            <Tilt><img src={node} alt="node"/></Tilt>
-            <p>NodeJS</p>
-            </div>
-            
-            <div className="tecnologia">
-            <Tilt><img src={mongo} alt="mongo"/></Tilt>
-            <p>MongoDB</p>
-            </div>
-
-            <div className="tecnologia"><Tilt>
-            <img src={mysql} alt="mysql"/></Tilt>
-            <p>Mysql</p>
-            </div>
-
-            <div className="tecnologia"><Tilt>
-            <img src={csharp} alt="C#"/></Tilt>
-            <p>C#</p>
-            </div>
-
-            <div className="tecnologia"><Tilt>
-            <img src={dotnet} alt=".Net"/></Tilt>
-            <p>.Net</p>
-            </div>
+                    {/* Ring for percentage */}
+                    <svg className="skill-ring" viewBox="0 0 100 100">
+                        <circle className="ring-bg" cx="50" cy="50" r="45" />
+                        <circle
+                            className="ring-fill"
+                            cx="50" cy="50" r="45"
+                            style={{ strokeDasharray: `${(level * 283) / 100} 283` }}
+                        />
+                    </svg>
+                    <span className="skill-percent">{level}%</span>
+                </div>
+            </Tilt>
+            <p>{name}</p>
         </div>
-        </Container>
-        </div>
-        </>
     )
 }
